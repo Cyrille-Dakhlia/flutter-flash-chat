9273a07dd4a9c5bdd681f474dcedf44000d41e9b
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2023-04-11 11:41:03 +0200

    feat: add progress indicator when login or registering

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String routeId = 'registration_screen';
@@ -20,64 +21,71 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
+      body: ProgressHUD(
+        child: Builder(builder: (context) {
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                TextField(
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) => email = value,
+                  decoration: kTextFieldDecorationBlueAccent.copyWith(
+                      hintText: 'Enter your email'),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                  onChanged: (value) => password = value,
+                  decoration: kTextFieldDecorationBlueAccent.copyWith(
+                      hintText: 'Enter your password'),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                RoundedButton(
+                  label: 'Register',
+                  color: Colors.blueAccent,
+                  onPressed: () async {
+                    final progress = ProgressHUD.of(context);
+                    progress?.show();
+                    try {
+                      var userCredential =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: email, password: password);
+                      if (userCredential.user != null) {
+                        Navigator.pushNamed(context, ChatScreen.routeId);
+                      }
+                    } catch (e) {
+                      print(e);
+                      String message =
+                          'Oops! Make sure to choose a validate email address, and choose a password of at least 6 characters.';
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(message)));
+                    }
+                    progress?.dismiss();
+                  },
+                ),
+              ],
             ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) => email = value,
-              decoration: kTextFieldDecorationBlueAccent.copyWith(
-                  hintText: 'Enter your email'),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) => password = value,
-              decoration: kTextFieldDecorationBlueAccent.copyWith(
-                  hintText: 'Enter your password'),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              label: 'Register',
-              color: Colors.blueAccent,
-              onPressed: () async {
-                try {
-                  var userCredential =
-                      await _auth.createUserWithEmailAndPassword(
-                          email: email, password: password);
-                  if (userCredential.user != null) {
-                    Navigator.pushNamed(context, ChatScreen.routeId);
-                  }
-                } catch (e) {
-                  print(e);
-                  String message =
-                      'Oops! Make sure to choose a validate email address, and choose a password of at least 6 characters.';
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text(message)));
-                }
-              },
-            ),
-          ],
-        ),
+          );
+        }),
       ),
     );
   }
