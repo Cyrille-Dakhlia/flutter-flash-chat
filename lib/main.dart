17066be170a5efe659556e5e8862d75a43300241
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2023-04-12 17:52:12 +0200

    feat: setup Firebase Local Emulators Suite (localhost:4000), with initial dataset, and script to run Firebase Authentication and Cloud Firestore emulators importing the initial dataset (emulators run on debug mode only)

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  if (kDebugMode) {
+    // Use custom script ./run_firebase_emulators_data
+    try {
+      FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+      await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    } catch (e) {
+      print(e);
+    }
+  }
+
   runApp(FlashChat());
 }
 
